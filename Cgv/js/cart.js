25a85d4285e68a96c06f4d9712bf15556b9583c7
salamence2019 <skeldkok@gmail.com>
2023-08-29 21:03:52 +0900

    CSS 힘드러ㅠㅠ

--- Cgv/js/cart.js
+++ Cgv/js/cart.js
@@ -1,91 +1,90 @@
 let cartList = JSON.parse(localStorage.getItem("allItem"));
 
-console.log(cartList.length);
+//console.log(cartList.length);
 function listing() {
-  if (cartList.length) {
-    let priceTotal = 0;
-    $(".cartInfo p").text("");
-    let trList = `
-    
-    <table border="1">
-                  <colgroup>
-                    <col />
-                    <col />
-                    <col />
-                    <col />
-                    <col />
-                  </colgroup>
-                <thead>
-                  <tr>
-                    <th>이미지</th>
-                    <th>상품정보</th>
-                    <th>수량</th>
-                    <th>금액</th>
-                    <th>선택</th>
-                  </tr>
-                </thead>`;
-    trList += `<tbody>`;
-    cartList.forEach((value, index) => {
-      trList += `<tr>`;
-      trList += `<td><img src="${value.image}" alt="${value.productName}" style="width:80px" /></td>`;
-      trList += `<td>${value.productName} <br> <span class="price">${value.price}</span></td>`;
-      trList += `<td style="display: inline">
-                <button type="button" class="qty__plus">+</button>
-                <input type="text" value="${value.count}" autocomplete="off" class="qty__input">
-                <button type="button" class="qty__minus">-</button>
-             </td>`;
-      trList += `<td class="myTotal">${(
-        parseInt(value.price.replace(",", "")) * parseInt(value.count)
-      )
-        .toString()
-        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>`;
-      trList += `<td><button type="button" class="remove">삭제</button></td>`;
-      trList += `</tr>`;
-      priceTotal =
-        parseInt(value.price.replace(",", "")) * parseInt(value.count) +
-        priceTotal;
-    });
-    trList += `</tbody>`;
-    trList += `<tfoot>`;
-    trList += `<tr>`;
-    trList += `<td colspan="5">합계 : <span>${priceTotal
-      .toString()
-      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</span></td>`;
-    trList += `</tr>`;
-    trList += `</tfoot>`;
-    trList += `</table>`;
-    trList += `<div class="order"><button type=button>주문하기</button></div>
-   
-    `;
+  $("#cartHeader").append(`
+  <header></header>
+  <h1>장바구니</h1>
+  <div class="shopping-cart">
+    <div class="column-labels">
+      <label class="product-image product-name">상품이미지</label>
+      <label class="product-details">상품</label>
+      <label class="product-price">가격</label>
+      <label class="product-quantity">개수</label>
+      <label class="product-removal">삭제</label>
+      <label class="product-line-price">합계</label>
+    </div>
+  `);
 
-    $(".cartBox table").remove();
-    $(".cartBox .order").remove();
-    $(".cartBox").append(trList);
-  } else {
-    $(".cartInfo p").text("장바구니가 비어있습니다.");
-    $(".cartBox table").remove();
-  }
+  if (cartList.length) {
+    let grandTotal=0;
+  
+  cartList.forEach(productInfo  => {
+    $("#cartFor").append(`
+    <div class="product">
+      <div class="product-image">
+        <img src="${productInfo.image}">
+      </div>
+      <div class="product-details">
+        <div class="product-title">${productInfo.productName}</div>
+        <p class="product-description">${productInfo.productName}</p>
+      </div>
+      <div class="product-price">${productInfo.price}</div>
+
+      <div class="product-quantity">
+      <a href="#none" class="com_btn_minus qty__minus">-</a>
+      <span class="com_form_count com_form_count0">${productInfo.count}</span>
+      <a href="#none" class="com_btn_plus qty__plus">+</a>
+      </div>
+
+      <div class="product-removal">
+        <button class="remove-product">
+          삭제
+        </button>
+      </div>
+      <div class="product-line-price">${(productInfo.price.toString().replace(",","") * productInfo.count).toString().replace(/\B(?=(\d{3})+(?!\d))/g,",")}</div>
+    </div>
+    `);
+    grandTotal = (productInfo.price.toString().replace(",","") * productInfo.count) + grandTotal; 
+  });
+  
+}
+$("#cartTotal").append(`
+     
+  <div class="totals">
+    <div class="totals-item totals-item-total">
+      <label>합계</label>
+      <div class="totals-value" id="cart-total">${grandTotal.toString().replace(/\B(?=(\d{3})+(?!\d))/g,",")}</div>
+    </div>
+  </div>
+      <button class="checkout">결제</button>
+<script>
+$("header").load("./header.html");
+$("footer").load("./footer.html");
+</script>
+`)
 }
 
 listing();
 
 $("body").on("click", ".qty__plus", function () {
+  
   // $(".qty__input").val() = input 상자에 입력된 값 문자로 추출(parseInt로 묶으면 문자는 무시하고 숫자로 변환해줌)
-  let count = $(this).next().val(); // 지역변수quantity에 this(클릭한qty__plus)의 next(다음요소의 값(input))을 추출해서 대입
-  let myprice = $(this).parent().prev().find(".price").text();
+  let count = $(this).prev().text(); // 지역변수quantity에 this(클릭한qty__plus)의 next(다음요소의 값(input))을 추출해서 대입
+  let myprice = $(this).parent().prev().text();
   let total = 0;
-  if (count) {
+  if (count) {  
     count = count;
-    $(this).next().val(++count); // select, input에 값을 넣어줄때 val(값)(val() 빈칸일 때는 현재값을 추출해줌)
+    $(this).prev().text(++count); // select, input에 값을 넣어줄때 val(값)(val() 빈칸일 때는 현재값을 추출해줌)
     total = count * parseInt(myprice.toString().replace(",", ""));
   } else {
     count = 1;
-    $(this).next().val(count);
+    $(this).prev().text(count)
     total = count * parseInt(myprice.toString().replace(",", ""));
   }
   $(this)
     .parent()
-    .next()
+    .next().next()
     .text(total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
 
   getTotal();
@@ -93,20 +92,20 @@ $("body").on("click", ".qty__plus", function () {
 
 // - 누르면
 $("body").on("click", ".qty__minus", function () {
-  count = parseInt($(this).prev().val()); // prev = 이전요소 // 지역변수quantity에 this(클릭한qty__minus)의 prev(이전요소의 값(input))을 추출해서 대입
-  let myprice = $(this).parent().prev().find(".price").text();
+  count = parseInt($(this).next().text()); // prev = 이전요소 // 지역변수quantity에 this(클릭한qty__minus)의 prev(이전요소의 값(input))을 추출해서 대입
+  let myprice = $(this).parent().prev().text();
   let total = 0;
   if (count > 1) {
-    $(this).prev().val(--count);
+    $(this).next().text(--count);
     total = count * parseInt(myprice.toString().replace(",", ""));
   } else {
     count = 1;
-    $(this).prev().val(count);
+    $(this).next().text(count);
     total = count * parseInt(myprice.toString().replace(",", ""));
   }
   $(this)
     .parent()
-    .next()
+    .next().next()
     .text(total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
 
   getTotal();
@@ -123,12 +122,12 @@ function getTotal() {
   $("tfoot span").text(total);
 }
 
-$("body").on("click", "tbody .remove", function () {
-  let tRnum = $(this).parent().parent().index();
-  //console.log(tRnum);
+$("body").on("click", ".remove-product", function () {
+  let tRnum = $(this).parent().parent().parent().parent().index();
+  console.log(tRnum);
   cartList.splice(tRnum, 1);
+  
   localStorage.setItem("allItem", JSON.stringify(cartList));
-
   listing();
 });
 
